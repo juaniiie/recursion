5989f89afc834af15f9fe28a7300754a6683582a
Juana <becjuana@gmail.com>
2015-08-03 23:43:00 -0700

    Improved getElementClassName code: more succinct

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,27 +4,18 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className) {
-  var args = className.split(' ');
-  var nodelist = [];
-  var findClass = function(element, className) {
-    for (var i = 0; i < element.childNodes.length; i++) {
-      var contains = true;
-      for (var j = 0; j < args.length; j++) {
-        if (element.childNodes[i].classList === undefined) {
-          contains = false;
-        }else if (!element.childNodes[i].classList.contains(args[j])) {
-          contains = false;
-        }
-      }
-      if (contains) {
-        nodelist = nodelist.concat(element.childNodes[i]);
-      }
-      if (element.childNodes[i].childNodes.length > 0) {
-        findClass(element.childNodes[i], args);
-      }
+
+var getElementsByClassName = function(className, nodes) {
+  var elementClassArr = [];
+  var node = nodes || document.body;
+  if (node.classList && node.classList.contains(className)) {
+    elementClassArr.push(node);
+  }
+  if (node.childNodes.length !== 0) {
+    for (var i = 0; i < node.childNodes.length; i++) {
+      elementClassArr = elementClassArr.concat(getElementsByClassName(className, node.childNodes[i]));
     }
-  };
-  findClass(document, args);
-  return nodelist;
+  }
+  return elementClassArr;
 };
+
